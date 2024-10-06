fdfe9266e5b8cec65dba19d6b1a309677e86059d
an0n99 <jgr92@proton.me>
2024-10-06 14:20:31 +0100

    Update TokenCreate.jsx

--- frontend/src/components/TokenCreate.jsx
+++ frontend/src/components/TokenCreate.jsx
@@ -30,15 +30,13 @@ const TokenCreate = () => {
   return (
     <div className="app">
       <nav className="navbar">
-        <a href="#" className="nav-link">[moralis]</a>
-        <a href="#" className="nav-link">[docs]</a>
         <button className="nav-button">[connect wallet]</button>
       </nav>
       <div className="token-create-container">
       <h3 className="start-new-coin" onClick={() => navigate('/')}>[go back]</h3>
-        <p className="info-text">MemeCoin creation fee: 0.0001 ETH</p>
+        <p className="info-text">MemeCoin creation fee: 0.01 ORE</p>
         <p className="info-text">Max supply: 1 million tokens. Initial mint: 200k tokens.</p>
-        <p className="info-text">If funding target of 24 ETH is met, a liquidity pool will be created on Uniswap.</p>
+        <p className="info-text">If funding target of 24 ORE is met, a liquidity pool will be created on Raydium.</p>
         <div className="input-container">
           <input
             type="text"
